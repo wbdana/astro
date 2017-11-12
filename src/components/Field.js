bc2ba29c86e6b3b87a83f8321b0a31326d23bef7
github username <github email address>
2017-11-12 17:03:56 -0500

    Create Field; begin drawing Ship

--- src/components/Field.js
+++ src/components/Field.js
@@ -3,13 +3,63 @@ import Ship from './Ship'
 
 class Field extends React.Component {
   state = {
-    ship: {},
+    ship: {
+      pos: {
+        x: 500,
+        y: 500,
+        d: 0
+      },
+      vel: {
+        x: 0,
+        y: 0
+      }
+    },
     asteroids: []
+  }
+
+  componentDidMount() {
+    // console.log(this.context)
+    // setTimeout(
+    //   this.setState({
+    //     ship: {
+    //       pos: {
+    //         x: 900,
+    //         y: 567,
+    //         d: 90
+    //       },
+    //       vel: {
+    //         x: 20,
+    //         y: 20
+    //       }
+    //     }
+    //   }),
+    // 3500)
+    window.addEventListener('keydown', (event) => {
+      console.log(event.key)
+      if (event.key === 'a') {
+        this.setState({
+          ship: {
+            pos: {
+              d: 270
+            },
+            ...this.state.ship
+          }
+        }, ()=>{console.log(this.state.ship)})
+      }
+    })
   }
 
   render() {
     return(
-      <canvas className='Field'>
+      <canvas
+        id='AstroField'
+        width='1905px'
+        height='961px'
+      >
+        <Ship
+          pos={this.state.ship.pos}
+          vel={this.state.ship.vel}
+        />
       </canvas>
     )
   }
--- src/components/Ship.js
+++ src/components/Ship.js
@@ -2,18 +2,59 @@ import React from 'react'
 
 class Ship extends React.Component {
   state = {
-    ship: {
-      positionX: 400,
-      positionY: 20,
-      vector: 0
+    pos: {
+      x: this.props.pos.x,
+      y: this.props.pos.y,
+      d: this.props.pos.d
+    },
+    vel: {
+      x: this.props.vel.x,
+      y: this.props.vel.y
     }
+  }
+
+  componentDidMount() {
+    // console.log(this.props)
+    // console.log(this.state.context)
+    // console.log(this.state)
+    // console.log(this.context)
+    this.drawShip()
+  }
+
+  componentWillReceiveProps() {
+    let ctx = document.getElementById('AstroField').getContext('2d')
+    ctx.restore()
+  }
+
+  drawShip = () => {
+      let c = document.getElementById('AstroField')
+      console.log(c)
+      let ctx = c.getContext('2d')
+      console.log(ctx)
+      ctx.strokeStyle = '#FFFFFF'
+      ctx.fillStyle = '#000000'
+      ctx.lineWidth = 2;
+      // ctx.beginPath()
+      ctx.moveTo(this.state.pos.x, this.state.pos.y)
+      // draw left side
+      ctx.lineTo(this.state.pos.x - 5, this.state.pos.y + 17)
+      // reset point
+      ctx.moveTo(this.state.pos.x, this.state.pos.y)
+      // draw right side
+      ctx.lineTo(this.state.pos.x + 5, this.state.pos.y + 17)
+      // TODO: draw arc
+      // ctx.closePath()
+      ctx.fill()
+      ctx.stroke()
+      ctx.restore()
   }
 
   render() {
     return(
       <div className='Ship'>
-
       </div>
     )
   }
 }
+
+export default Ship
