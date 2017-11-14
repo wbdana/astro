bb94bf270dde621c3c585d6015c6b576dd696bf4
github username <github email address>
2017-11-13 20:42:20 -0500

    Implement proof of concept Ship momentum

--- src/components/Ship.js
+++ src/components/Ship.js
@@ -14,10 +14,23 @@ class Ship extends React.Component {
   }
 
   componentDidMount() {
-    console.log("DidMount")
+    // console.log("DidMount")
 
-    // Redraw ship every 20ms (TODO: Check that this actually works)
-    setTimeout(this.drawShip(), 20)
+    // Redraw ship every 20ms, applying velocity to position each time
+    //(TODO: Check that this actually works)
+    setInterval(() => {
+      this.setState({
+        pos: {
+          ...this.state.pos,
+          x: this.state.pos.x + this.state.vel.x,
+          y: this.state.pos.y + this.state.vel.y,
+        },
+        vel: {
+          ...this.state.vel
+        }
+      })
+      this.drawShip()
+    }, 5)
 
     // Add keydown event listener to facilitate control
     window.addEventListener('keydown', (event) => {
@@ -50,36 +63,97 @@ class Ship extends React.Component {
         }, ()=>{console.log(this.state)})
       }
       // If keypress is w (forward), calculate x- and y-components of current
-      // vector
-      // The current value of pixels (force) to be added per keypress is 10
+      // vector and add them to velocity
+      // The current value of pixels (force) to be added per keypress is 1
       // (see multiplier below)
       if (keypress === 'w') {
         console.log("Current this.state.pos.d: ", this.state.pos.d)
+        // this.updateOrLimitVelocity()
         this.setState({
           pos: {
             ...this.state.pos
           },
           vel: {
-            x: this.state.vel.x + (10*(Math.cos(this.state.pos.d*Math.PI/180))),
-            y: this.state.vel.y + (10*(Math.sin(this.state.pos.d*Math.PI/180)))
+            x: this.state.vel.x + (0.5*(Math.sin(this.state.pos.d*Math.PI/180))),
+            y: this.state.vel.y - (0.5*(Math.cos(this.state.pos.d*Math.PI/180)))
           }
-        }, ()=>{console.log(this.state.vel)})
+        }, ()=>{console.log(this.state.vel.x);console.log(this.state.vel.y)})
       }
-
     })
   }
 
-  // componentWillReceiveProps() {
-  //   let ctx = document.getElementById('AstroField').getContext('2d')
-  //   ctx.restore()
-  // }
-
-  // componentDidUpdate() {
-  //   this.drawShip()
-  // }
+  updateOrLimitVelocity = () => {
+    console.log("updateOrLimitVelocity()")
+    let newVelX = this.state.vel.x + (0.5*(Math.sin(this.state.pos.d*Math.PI/180)))
+    let newVelY = this.state.vel.y - (0.5*(Math.sin(this.state.pos.d*Math.PI/180)))
+    if (newVelX >= 2 && this.state.vel.x > 0) {
+      this.setState({
+        pos: {
+          ...this.state.pos
+        },
+        vel: {
+          ...this.state.vel,
+          x: 2
+        }
+      })
+    } else if (newVelX <= -2 && this.state.vel.x < 0) {
+      this.setState({
+        pos: {
+          ...this.state.pos
+        },
+        vel: {
+          ...this.state.vel,
+          x: -2
+        }
+      })
+    } else {
+      this.setState({
+        pos: {
+          ...this.state.pos
+        },
+        vel: {
+          ...this.state.vel,
+          x: newVelX
+        }
+      })
+    }
+    if (newVelY >= 2 && this.state.vel.y > 0) {
+      this.setState({
+        pos: {
+          ...this.state.pos
+        },
+        vel: {
+          ...this.state.vel,
+          y: 2
+        }
+      })
+    } else if (newVelY <= -2 && this.state.vel.y < 0){
+      this.setState({
+        pos: {
+          ...this.state.pos
+        },
+        vel: {
+          ...this.state.vel,
+          y: -2
+        }
+      })
+    } else {
+      this.setState({
+        pos: {
+          ...this.statepos
+        },
+        vel: {
+          ...this.state.vel,
+          y: newVelY
+        }
+      })
+    }
+    console.log(this.state.vel.x)
+    console.log(this.state.vel.y)
+  }
 
   drawShip = () => {
-      console.log("drawShip()")
+      // console.log("drawShip()")
       let c = document.getElementById('AstroField')
       let ctx = c.getContext('2d')
       let angle = this.state.pos.d
@@ -96,11 +170,11 @@ class Ship extends React.Component {
       // console.log("Rotated")
       ctx.beginPath()
       // ctx.move()
-      ctx.moveTo(0,0)
-      ctx.lineTo(-5,17)
-      ctx.lineTo(-4,14)
-      ctx.lineTo(4,14)
-      ctx.lineTo(5,17)
+      ctx.moveTo(0,-7)
+      ctx.lineTo(-5,15)
+      ctx.lineTo(-4,12)
+      ctx.lineTo(4,12)
+      ctx.lineTo(5,15)
       // ctx.moveTo(this.state.pos.x, this.state.pos.y)
       // ctx.lineTo(this.state.pos.x - 5, this.state.pos.y + 17)
       // ctx.lineTo(this.state.pos.x - 4, this.state.pos.y + 14)
@@ -113,12 +187,12 @@ class Ship extends React.Component {
   }
 
   componentDidUpdate() {
-    console.log("DidUpdate")
+    // console.log("DidUpdate")
     this.drawShip()
   }
 
   render() {
-    console.log("RENDERING")
+    // console.log("RENDERING")
     return(
       <div className='Ship'>
 
