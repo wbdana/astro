6ba6c16480aec3408811724999380fc543ada604
github username <github email address>
2017-11-15 20:28:35 -0500

    Add Asteroid; add planning note

---
+++ src/components/Asteroid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Asteroid extends React.Component {
+  // 8 sides
+  //   Random length, random angle (off of vertical, where this angle is greater than the prior angle)
+  //   ctx.closePath()
+}
+
+export default Asteroid
