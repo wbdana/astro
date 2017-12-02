2ecb8b6554c7872e9146a5190dd6c97def4f350b
github username <github email address>
2017-12-02 18:19:50 -0500

    Work on Asteroid breakups

--- src/components/AsteroidContainer.js
+++ src/components/AsteroidContainer.js
@@ -92,101 +92,11 @@ class AsteroidContainer extends React.Component {
     return(0)    
   }
 
-  // createAsteroid = (size) => {
-  //   // let numSides = 8
-  //   let numSides = getRandomIntInclusive(7,9)
-  //   let min = 0
-  //   let angles = []
-  //   let sides = []
-  //   let posX = getRandomIntInclusive(5,200) // TODO - move away from starting pos of ship
-  //   let posY = getRandomIntInclusive(5,1000)
-  //   let posD = getRandomIntInclusive(0,359)
-  //   let velX = getRandomIntInclusive(-5,5)
-  //   let velY = getRandomIntInclusive(-5,5)
-  //   let newAngle = 45
-  //   let i
-  //   let newSideMultiplier
-  //   for (i = 0; i < numSides; i++) {
-  //     // newAngle = getRandomIntInclusive(45, 45)
-  //     newSideMultiplier = getRandomIntInclusive(70,80)
-  //     angles.push(newAngle)
-  //     sides.push(size*newSideMultiplier)
-  //     // min = newAngle
-  //   }
-  //   let newAsteroid = {
-  //     angles: angles,
-  //     sides: sides,
-  //     pos: {
-  //       x: posX,
-  //       y: posY,
-  //       d: posD
-  //     },
-  //     vel: {
-  //       x: velX,
-  //       y: velY // TODO: should add spin
-  //     },
-  //     size: size
-  //   }
-  //   this.props.createAsteroids(newAsteroid)
-  //   return(0)
-  // }
-
   updateAndConfineAsteroidToField = (id) => {
-    // CONFINE ASTEROID TO FIELD
-
-    // If Asteroid goes off screen bottom right corner,
-    // come out top left corner
-
-    // console.log(this.props.asteroids)
-
-    // if (((this.props.asteroids[id].pos.x + this.props.asteroids[id].vel.x) >= window.innerWidth) && ((this.props.asteroids[id].pos.y + this.props.asteroids[id].vel.y) >= window.innerHeight)) {
-    //   this.props.adjustTopLeft(id)
-    // }
-
-    // // If Asteroid goes off screen bottom left corner, come out on top right side
-    // else if (((this.props.asteroids[id].pos.x + this.props.asteroids[id].vel.x) <= 0) && ((this.props.asteroids[id].pos.y + this.props.asteroids[id].vel.y) >= window.innerHeight)) {
-    //   this.props.adjustTopRight(id)
-    // }
-
-    // // If Asteroid goes off screen top right corner, come out on bottom left corner
-    // else if (((this.props.asteroids[id].pos.x + this.props.asteroids[id].vel.x) >= window.innerWidth) && ((this.props.asteroids[id].pos.y + this.props.asteroids[id].vel.y) <= 0)) {
-    //   this.props.adjustBottomLeft(id)
-    // }
-
-    // // If Asteroid goes off screen top left corner, come out on bottom right corner
-    // else if (((this.props.asteroids[id].pos.x + this.props.asteroids[id].vel.x) <= 0) && ((this.props.asteroids[id].pos.y + this.props.asteroids[id].vel.y) <= 0)) {
-    //   this.props.adjustBottomRight(id)
-    // }
     console.log(this.props.asteroids)
     console.log(id)
     console.log(this.props.asteroids[id].pos)
     this.props.updateAsteroidLocation(id)
-    // // If Asteroid goes off screen right, come out on left side
-    // if ((this.props.asteroids[id].pos.x + this.props.asteroids[id].vel.x) >= window.innerWidth) {
-    //   this.props.adjustLeft(id)
-    // }
-
-    // // If Asteroid goes off screen left, come out on right side
-    // else if ((this.props.asteroids[id].pos.x + this.props.asteroids[id].vel.x) <= 0) {
-    //   this.props.adjustRight(id)
-    // }
-
-    // // If Asteroid goes off screen bottom, come out on top side
-    // else if ((this.props.asteroids[id].pos.y + this.props.asteroids[id].vel.y) >= window.innerHeight) {
-    //   this.props.adjustTop(id)
-    // }
-
-    // // If Asteroid goes off screen top, come out on bottom side
-    // else if ((this.props.asteroids[id].pos.y + this.props.asteroids[id].vel.y) <= 0) {
-    //   this.props.adjustBottom(id)
-    // }
-
-    // // Or, if we are within the boundaries already...
-    // else {
-    //   this.props.updateAsteroidLocation(id)
-    // }
-
-    // return(0) // for async purposes
   }
 
   render() {
